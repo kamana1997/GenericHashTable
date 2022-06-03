f20b710344eadeeb131201c585ee1edd33e01903
Hardik Agarwal <hardikagarwal1703@gmail.com>
2022-06-03 17:11:30 +0530

    Added Main function and header syso.

---
+++ src/com/cdac/ads/main/HashAppMain.java
@@ -0,0 +1,30 @@
+/*
+ * @Author: 
+ * @ProjectTitle: ADS-Mini Project 
+ * @ProblemStatement: Implementation of Generic Hash Table with chaining by AVL Tree
+ * @Date: 01-06-2022 -> 05-06-2022
+ */
+
+package com.cdac.ads.main;
+
+public class HashAppMain {
+
+	public static void main(String[] args) {
+		header();
+	}
+
+	//function to add header while execution.
+	public static void header() {
+		System.out.println("ADS MINI PROJECT ~ Hardik(220340120083) & Joy(220340120092)");
+		System.out.println("Implementation of Generic Hash Table with chaining by AVL Tree as collision handling");
+		System.out.println();
+		System.out.println("Project Specifications:");
+		System.out.println("Key Type: String");
+		System.out.println("Value: <Generic>");
+		System.out.println("Default Load Factor: 0.75");
+		System.out.println("Default BucketArray Size: 10");
+		System.out.println("Prepopulated Elements for user convenience: n = 5");
+		System.out.println();
+	}
+
+}
