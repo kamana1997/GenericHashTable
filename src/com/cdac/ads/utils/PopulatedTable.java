dc2d102484ab3a2467c2aa227cc14129b3763b7f
uk2311 <keskarudit@gmail.com>
2022-06-06 22:30:56 +0530

    Custom Exception and Populated String Map

--- src/com/cdac/ads/utils/PopulatedTable.java
+++ src/com/cdac/ads/utils/PopulatedTable.java
@@ -1,9 +1,22 @@
 package com.cdac.ads.utils;
 
+import com.cdac.ads.hashtable.HashTable;
+
 public class PopulatedTable {
 	
 	public void populateStringMap() {
 		//TODO
+		HashTable<String> hT = new HashTable<>();
+		hT.add(1,"S1");
+		hT.add(6,"S6");
+		hT.add(9,"S9");
+		hT.add(7,"S7");
+		hT.add(5,"S5");
+		hT.add(2,"S2"); 
+		hT.add(3,"S3"); 
+		hT.add(4,"S4");
+		hT.add(8,"S8");
+		hT.add(10,"S10");
 	}
 	
 	public void populateIntegerMap() {
