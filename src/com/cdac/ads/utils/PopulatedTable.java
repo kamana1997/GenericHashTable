07533e56def7ac87aada008079abdf7171a0090f
Hardik Agarwal <hardikagarwal1703@gmail.com>
2022-06-07 00:55:22 +0530

    Removed TODO

--- src/com/cdac/ads/utils/PopulatedTable.java
+++ src/com/cdac/ads/utils/PopulatedTable.java
@@ -3,16 +3,14 @@ package com.cdac.ads.utils;
 import com.cdac.ads.hashtable.HashTable;
 
 public class PopulatedTable {
-
-	//TODO @Udit please make populating data.  
+ 
 	public HashTable<String> populateStringMap(HashTable<String> hTS) {
 		hTS.add(1,"Joy");
 		hTS.add(6,"Hardik");
 		
 		return hTS;
 	}
-
-	//TODO @Udit please make populating data.  
+ 
 	public HashTable<Integer> populateIntegerMap(HashTable<Integer> hTI) {
 		hTI.add(1, 9876);
 		hTI.add(9, 5432);
@@ -20,7 +18,6 @@ public class PopulatedTable {
 		return hTI;
 	}
 	
-	//TODO @Udit please make populate double.
 	public HashTable<Double> populateDoubleMap(HashTable<Double> hTD) {
 		hTD.add(2, 9342.75);
 		hTD.add(1, 3021.50);
