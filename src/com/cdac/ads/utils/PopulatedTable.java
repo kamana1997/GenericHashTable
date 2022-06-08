763a86c6c80f078d4b068ac274ccd7cd6ecc7ede
hardikagarwal1703 <104696034+hardikagarwal1703@users.noreply.github.com>
2022-06-08 15:22:44 +0530

    Update PopulatedTable.java

--- src/com/cdac/ads/utils/PopulatedTable.java
+++ src/com/cdac/ads/utils/PopulatedTable.java
@@ -14,11 +14,11 @@ public class PopulatedTable {
 	// Populated data for string type values
 	public HashTable<String> populateStringMap(HashTable<String> hTS) {
 		// hTS.add(key(Integer), value(String))
-		hTS.add(1, "Ajay");
 		hTS.add(6, "Mahesh");
-		hTS.add(3, "Kanika");
+		hTS.add(23, "Kanika");
 		hTS.add(7, "Sameer");
 		hTS.add(13, "Harshita");
+		hTS.add(33, "Harshita");
 
 		return hTS;
 	}
@@ -27,9 +27,9 @@ public class PopulatedTable {
 	public HashTable<Integer> populateIntegerMap(HashTable<Integer> hTI) {
 		// hTI.add(key(Integer), value(Integer))
 		hTI.add(1, 9876);
-		hTI.add(9, 5432);
 		hTI.add(19, 7331);
-		hTI.add(5, 6434);
+		hTI.add(9, 5432);
+		hTI.add(29, 6434);
 		hTI.add(15, 9935);
 
 		return hTI;
@@ -39,9 +39,9 @@ public class PopulatedTable {
 	public HashTable<Double> populateDoubleMap(HashTable<Double> hTD) {
 		// hTI.add(key(Integer), value(Double))
 		hTD.add(2, 9342.75);
-		hTD.add(1, 3021.50);
-		hTD.add(3, 4023.89);
-		hTD.add(7, 6425.70);
+		hTD.add(12, 3021.50);
+		hTD.add(22, 4023.89);
+		hTD.add(32, 6425.70);
 		hTD.add(13, 9956.56);
 
 		return hTD;
